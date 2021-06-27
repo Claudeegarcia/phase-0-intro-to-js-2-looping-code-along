75b965a0184abdaeaf7b2572682221852e88204d
Claudia <claudeegarcia@icloud.com>
2021-06-27 00:27:54 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+
+function writeCards(name){ 
+    let message = []
+    for (let i = 0; i < name.length; i++){
+        message.push(`Thank you, ${name[i]}, for the wonderful surprise gift!`);
+    }
+    return message;
+}
+
+
+function countDown(i){
+    while(i >= 0) {
+        console.log(i)
+        i--;
+    }
+}
+
